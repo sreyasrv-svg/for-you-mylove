acf8f2f277f96d13f4ec2c0c54e73438cc90d957
sreyasrv-svg <sreyasrvinod2210@gmail.com>
2026-01-28 19:42:54 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ const SIMRAN_DATA = {
         },
         { 
             title: "Forever & Always ❤️", 
-            msg: "Happy Valentine's Day ❤️, Simran! You're the brightest star in my sky. I'm so glad you're mine, today and for all time. All the stars align, when you say, you’re mine."
+            msg: "Happy Valentine's Day ❤️, Simran! You're the brightest star in my sky. I'm so glad you're mine, today and for all time. All the stars align when you say: 'You are mine.'"
         },
         {
             title: "", 
